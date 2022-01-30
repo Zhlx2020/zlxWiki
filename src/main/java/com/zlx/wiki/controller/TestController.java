c757445bb3bbbe2610ffc8bc91dc78ce707b1793
= <1844066342@qq.com>
2022-01-30 11:46:47 +0800

    0.1.3  使用httpClient测试get/post接口

--- src/main/java/com/zlx/wiki/controller/TestController.java
+++ src/main/java/com/zlx/wiki/controller/TestController.java
@@ -2,16 +2,29 @@ package com.zlx.wiki.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController // 返回字符串或json数据
 // @Controller 返回 一个 页面 前后端不分离用这个
 public class TestController {
-    // get post del put
+
+    /**
+     * 常用的 get post del put
+     * 普通请求 /hello?id=10
+     * restful /hello/10
+     */
     //@RequestMapping("/hello") // requestmapping 支持所有的请求方式 get也行 post也行
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello world";
+//    @GetMapping("/hello")
+//    public String hello(){
+//        return "hello world";
+//    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "this is post test helle," + name;
     }
 }
