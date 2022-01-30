de4b8ba788a95c232fabdbe3f7fcc058c0adee3f
= <1844066342@qq.com>
2022-01-30 10:52:19 +0800

    0.1.1 controller接口编写测试

---
+++ src/main/java/com/zlx/wiki/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.zlx.wiki.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 返回字符串或json数据
+// @Controller 返回 一个 页面 前后端不分离用这个
+public class TestController {
+    // get post del put
+    @RequestMapping("/hello") // requestmapping 支持所有的请求方式 get也行 post也行
+    public String hello(){
+        return "hello world";
+    }
+}
