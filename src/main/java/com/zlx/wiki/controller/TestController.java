cf3155550d1eada002abee1b1d9bb418a869dde9
= <1844066342@qq.com>
2022-01-30 10:53:49 +0800

    0.1.2  修改Testontroller接口只能使用get请求

--- src/main/java/com/zlx/wiki/controller/TestController.java
+++ src/main/java/com/zlx/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zlx.wiki.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 // @Controller 返回 一个 页面 前后端不分离用这个
 public class TestController {
     // get post del put
-    @RequestMapping("/hello") // requestmapping 支持所有的请求方式 get也行 post也行
+    //@RequestMapping("/hello") // requestmapping 支持所有的请求方式 get也行 post也行
+    @GetMapping("/hello")
     public String hello(){
         return "hello world";
     }
