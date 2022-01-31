470953511d1c882d7bd9ce4296ea8f9aaf908a5c
= <1844066342@qq.com>
2022-01-31 10:55:12 +0800

    0.5.8  增加ebook表

--- doc/all.sql
+++ doc/all.sql
@@ -16,3 +16,24 @@ create table `demo` (
 insert into demo (id, name) values
                                    (0, 'wxy'),
                                    (1, 'zlx');
+
+drop table if exists `ebook`;
+create table `ebook` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '书名',
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '预览数',
+    `vote_count` int comment '点赞数',
+    primary key (`id`)
+)engine = innodb default charset =utf8mb4 comment '电子书';
+
+insert into ebook (id, name)
+values (1,'深入浅出vue.js'),
+       (2,'高级程序设计js'),
+       (3,'深入理解es6'),
+       (4,'你不知道的JavaScript'),
+       (5,'http权威指南')
